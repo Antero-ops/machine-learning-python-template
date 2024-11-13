3ba94a9a06ab04ee2e46c4e99964fd5e88f6532b
Antero-ops <anterovasquez24@gmail.com>
2024-11-13 19:06:04 +0000

    cambios en explore.ipynb

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
 from utils import db_connect
+import pandas as pd
+
 engine = db_connect()
 
 # your code here
+
+train_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+test_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+
+print(total_data.head())
